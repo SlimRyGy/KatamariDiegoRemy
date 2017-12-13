a92f2eb9f0221e2125527c9dfedaf1e26d9c11e0
Unknown <barudy.diego@gmail.com>
2017-12-13 12:48:54 +0100

    AttractObject add in Player.cs

--- Assets/Team/Diego/Scripts/Player.cs
+++ Assets/Team/Diego/Scripts/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : DualBehavior
+public class Player : MonoBehaviour
 {
     #region Public Members
 
@@ -13,7 +13,11 @@ public class NewBehaviourScript : DualBehavior
     #endregion
 
     #region System
-
+    private void Reset()
+    {
+        // TODO mettre les collision entre Layer Player et Level à false
+        m_sphere_colider = GetComponent<SphereCollider>();
+    }
     void Start ()
     {
         
@@ -23,14 +27,26 @@ public class NewBehaviourScript : DualBehavior
     {
         	
     }
-
+    void OnCollisionEnter(Collision _col)
+    {
+        AttractObject(_col.gameObject);
+        
+    }
     #endregion
 
     #region Tools Debug and Utility
-
+    //ne pas détecter le sol
+    // detecter le type d'objet
+    private void AttractObject(GameObject _go) {
+        if (_go.tag == "AttractiveObject")
+        {
+            Debug.Log("collision obj");
+        }
+    }
     #endregion
 
     #region Private and Protected Members
-
+    [SerializeField]
+    private SphereCollider m_sphere_colider;
     #endregion
 }
