94873d951593c12fc5308e31381e33fc26c0b3af
Rygy <remy.grandry95@gmail.com>
2017-12-13 12:54:32 +0100

    Debut PlayerController

---
+++ Assets/Team/Remy/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerController : DualBehaviour 
+{
+    #region Public Members
+
+    public float m_movementSpeed;
+
+
+    #endregion
+
+    #region Public void
+
+    #endregion
+
+    #region System
+
+    private void Awake()
+    {
+        m_transform = GetComponent<Transform>();
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Start () 
+    {
+		
+	}
+	
+	void Update () 
+    {
+		
+	}
+
+    #endregion
+
+    #region Class Methods
+    
+    private void Move()
+    {
+        m_rigidbody.AddForce(new Vector3());
+    }
+    
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+    private Rigidbody m_rigidbody;
+
+    #endregion
+
+}
