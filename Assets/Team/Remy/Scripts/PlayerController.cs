1096d21e0271d5454797b6d367eddf417751fb8f
Unknown <remy.grandry95@gmail.com>
2017-12-14 14:29:52 +0100

    Correct Move and explor rotation

--- Assets/Team/Remy/Scripts/PlayerController.cs
+++ Assets/Team/Remy/Scripts/PlayerController.cs
@@ -35,10 +35,42 @@ public class PlayerController : DualBehaviour
 
     private void Move()
     {
-        float m_moveVertical    = Input.GetAxisRaw("Vertical");
-        Vector3 m_movement      = new Vector3(0.0f, 0.0f, m_moveVertical);
+        float m_moveVertical = Input.GetAxisRaw("Vertical");
+        float m_moveHorizontal = Input.GetAxisRaw("Horizontal");
+        //Vector3 m_movement      = new Vector3(0.0f, 0.0f, m_moveVertical);
 
-        m_rigidbody.AddForce(m_movement * m_movementSpeed, ForceMode.Force);
+        Vector3 camDirection = Camera.main.transform.forward;
+        camDirection.y = 0;
+        Vector3 camDirectionRight = Camera.main.transform.right;
+        camDirection.y = 0;
+
+        m_rigidbody.AddForce(camDirection * m_movementSpeed * m_moveVertical, ForceMode.Force);
+
+        m_rigidbody.AddForce(camDirectionRight * m_movementSpeed * m_moveHorizontal, ForceMode.Force);
+
+
+
+        /////
+        //float _moveHorizontal = Input.GetAxis("Horizontal");
+        //float _moveVertical = Input.GetAxis("Vertical");
+
+        //Vector3 _movement = new Vector3(_moveHorizontal, 0.0f, _moveVertical);
+
+        //Transform maCamera = Camera.main.transform;
+
+
+        ////  Quaternion.Lerp()
+
+        //Vector3 angle90 = new Vector3(0, 0, 0);
+        //Vector3 newDireciton = Quaternion.Euler(angle90) * maCamera.forward;
+
+
+
+
+        //m_rigidbody.AddForce(newDireciton * Time.deltaTime * _speed, _forceType);
+        //m_rigidbody.AddForce(maCamera.forward * Time.deltaTime * _speed, _forceType);`
+        ////
+
     }
     
     #endregion
