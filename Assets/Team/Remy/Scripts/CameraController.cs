a85859618e62720f93ef6143e6799a0d9fd1478e
Unknown <remy.grandry95@gmail.com>
2017-12-14 16:51:42 +0100

    On est revenu dans le temps :)

--- Assets/Team/Remy/Scripts/CameraController.cs
+++ Assets/Team/Remy/Scripts/CameraController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : DualBehaviour 
+public class CameraController : DualBehaviour
 {
     #region Public Members
 
@@ -13,7 +13,6 @@ public class CameraController : DualBehaviour
     public Transform m_cube;
     int angle = 0;
 
-    public Transform _affectedObject;
     #endregion
 
     #region Public void
@@ -22,22 +21,22 @@ public class CameraController : DualBehaviour
 
     #region System
 
-    void Start () 
+    void Start()
     {
-        m_offset = _affectedObject.position - m_player.transform.position;
-	}
+        m_offset = transform.position - m_player.transform.position;
+    }
 
     private void Update()
     {
-       // m_deltaRotation += Input.mouseScrollDelta.y;
+        // m_deltaRotation += Input.mouseScrollDelta.y;
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //transform.position = (m_player.transform.position + m_offset) + Quaternion.Euler(0,-1,0);
-            
+
             angle--;
             Debug.Log(angle);
             // transform.localPosition = m_transform.position + m_offset;
-            _affectedObject.localRotation = Quaternion.Euler(new Vector3(0f, angle, 0f));
+            transform.localRotation = Quaternion.Euler(new Vector3(0f, angle, 0f));
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
@@ -45,17 +44,17 @@ public class CameraController : DualBehaviour
 
             angle++;
             Debug.Log(angle);
-            _affectedObject.localRotation = Quaternion.Euler(new Vector3(0, angle, 0));
+            transform.localRotation = Quaternion.Euler(new Vector3(0, angle, 0));
         }
         if (Input.GetKeyDown(KeyCode.RightArrow)) { m_deltaRotationPos -= 1; }
-       // m_cube.localRotation = Quaternion.Euler(new Vector3(0, m_deltaRotation, 0));
+        // m_cube.localRotation = Quaternion.Euler(new Vector3(0, m_deltaRotation, 0));
 
     }
 
-    void LateUpdate () 
+    void LateUpdate()
     {
-        _affectedObject.position = Vector3.Lerp(_affectedObject.position, m_player.transform.position,Time.deltaTime); //+ m_offset * (m_player.transform.localScale.x/2),0.1f);
-	}
+        transform.position = Vector3.Lerp(transform.position, m_player.transform.position + m_offset * (m_player.transform.localScale.x / 2), 0.1f);
+    }
 
     #endregion
 
